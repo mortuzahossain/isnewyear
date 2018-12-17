f956ad9e1b1cb94b996e7c4fa986ae2bef752ba3
Mortuza Hossain <mortuzahossain1997@gmail.com>
2018-12-18 00:36:30 +0600

    Almost Finish

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template
+import datetime
+app = Flask(__name__)
+
+# Index Route
+@app.route('/')
+def index():
+    now = datetime.datetime.now()    
+    newYear = now.month == 1 and now.day == 1
+    # newYear = True
+    return render_template('index.html',new_year = newYear)
+
+if __name__ == '__main__':
+    app.run(debug = True,port = 3030)
+
